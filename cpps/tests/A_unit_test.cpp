46cfd953fb74e4eec272f1c325ae6072239f9c28
Zed ZHANG <xingxing0468@hotmail.com>
2020-10-04 14:04:31 +0800

    Init first triall cpp test

---
+++ cpps/tests/A_unit_test.cpp
@@ -0,0 +1,12 @@
+#include "cpps/includes/A.h"
+#include "gtest/gtest.h"
+
+TEST(A_FOO, EmptyOptionReturnWhenInputSmallerThan100) {
+  A a;
+  EXPECT_FALSE(a.Foo(90).has_value());
+}
+
+TEST(A_FOO, ReturnValueWhenInputLargerThan100) {
+  A a;
+  EXPECT_EQ(a.Foo(110), 100);
+}
