26b88446f53e9194e6867973d719408b697a2196
zZED <xingxing0468@hotmail.com>
2022-05-08 11:20:10 +0800

    remove debug log in CIS unit test

--- cpps/tests/lis_unit_test.cpp
+++ cpps/tests/lis_unit_test.cpp
@@ -9,10 +9,10 @@ TEST(LIS, Typical) {
   std::vector<int> seq{100, 80, 50, 60, 70, 65, 20, 30, 10, 40, 15, 60, 90, 25};
   std::vector<int> expected{20, 30, 40, 60, 90};
   auto ret = FindLongestIncreasingSubSequence(seq);
-  printf("Sequence: \n");
-  std::for_each(seq.begin(), seq.end(),
+  // printf("Sequence: \n");
+  // std::for_each(seq.begin(), seq.end(),
                 [](const int& num) { printf("%d ", num); });
-  printf("\n");
+  // printf("\n");
   EXPECT_EQ(expected, ret);
 }
 
