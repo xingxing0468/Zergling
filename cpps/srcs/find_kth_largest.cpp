4459c9ab86feea4c2062024ea45169efa4a5cfbf
Zed <xingxing0468@hotmail.com>
2022-05-28 11:50:27 +0800

    Impl of FindKthLargestWithHeapSort

--- cpps/srcs/find_kth_largest.cpp
+++ cpps/srcs/find_kth_largest.cpp
@@ -18,7 +18,7 @@ void insert_element_following_order(std::list<int>& collection, int value) {
 }
 
 int FindKthLargestWithPartialInsertSort(const std::vector<int>& nums, int k) {
-  if (nums.size() < k) {
+  if (nums.size() < k || k < 1) {
     return -1;
   }
   std::list<int> target_list{};
@@ -36,6 +36,59 @@ int FindKthLargestWithPartialInsertSort(const std::vector<int>& nums, int k) {
   return target_list.back();
 }
 
-int FindKthLargest(const std::vector<int>& nums, int k) {
-  return FindKthLargestWithPartialInsertSort(nums, k);
+void AdjustToKeepMaxHeapBelow(std::vector<int>& nums, int end_idx,
+                              int sub_max_heap_root_idx) {
+  // interate all the sub heap below to ensure parent is smallest
+  // Ensure after the swap the parent value is smaller in every sub heap
+  while (2 * sub_max_heap_root_idx + 1 < end_idx) {  // no left child
+    auto max_child_idx =
+        2 * sub_max_heap_root_idx + 2 >= end_idx  // has right child ?
+            ? 2 * sub_max_heap_root_idx + 1
+            : (nums[2 * sub_max_heap_root_idx + 1] >
+                       nums[2 * sub_max_heap_root_idx + 2]
+                   ? (2 * sub_max_heap_root_idx + 1)
+                   : (2 * sub_max_heap_root_idx + 2));
+    if (nums[max_child_idx] > nums[sub_max_heap_root_idx])  // current swap
+    {
+      auto tmp = nums[sub_max_heap_root_idx];
+      nums[sub_max_heap_root_idx] = nums[max_child_idx];
+      nums[max_child_idx] = tmp;
+      sub_max_heap_root_idx = max_child_idx;
+    } else {
+      break;
+    }
+  }  // end of while
+}
+
+void SetupMaxHeap(std::vector<int>& nums) {
+  for (int i = nums.size(); i > 0; --i) {
+    int parent_value = nums[(i - 1) / 2];
+    if (parent_value < nums[i]) {
+      // swap, move smaller one to parent pos
+      nums[(i - 1) / 2] = nums[i];
+      nums[i] = parent_value;
+      AdjustToKeepMaxHeapBelow(nums, nums.size(), i);
+    }
+  }
+}
+
+int FindKthLargestWithHeapSort(std::vector<int>& input, int order) {
+  if (order <= 0 || order > input.size()) {
+    return -1;
+  }
+  SetupMaxHeap(input);
+
+  // only need the max kth, so its enough just loop to input.size()) - order
+  for (int i = input.size() - 1; i >= static_cast<int>(input.size()) - order;
+       --i) {
+    // first swap the input[0] and input[i]
+    // to make the input[i] the max in [input[0], input[i]]
+    auto tmp = input[i];
+    input[i] = input[0];
+    input[0] = tmp;
+
+    // Have to adjust after swap to keep the min heap
+    AdjustToKeepMaxHeapBelow(input, i, 0);
+  }
+  return input[input.size() - order];
 }
